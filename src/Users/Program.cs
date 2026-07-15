fbb8c51a34f2cee44872d0b5ef5277d95b36e0b5
mohammad <mmdcj8499@gmail.com>
2026-07-15 11:10:39 +0330

    feat: add a custom exception to wrap up the lower level exceptions and implement the getting user by Id functionality

--- src/Users/Program.cs
+++ src/Users/Program.cs
@@ -1,9 +1,14 @@
+using UserService.Users.Application;
+using UserService.Users.Domain.Repositories;
 using UserService.Users.Infrastructure;
 using UserService.Users.Infrastructure.DbContext;
+using UserService.Users.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ApplicationDbContext>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService.Users.Application.UserService>();
 
 
 var app = builder.Build();
--- src/Users/Users.Infrastructure/Repositories/UserRepository.cs
+++ src/Users/Users.Infrastructure/Repositories/UserRepository.cs
@@ -1,7 +1,10 @@
+using System.Net.Sockets;
 using Dapper;
+using Npgsql;
 using UserService.Users.Domain.Models;
 using UserService.Users.Domain.Repositories;
 using UserService.Users.Infrastructure.DbContext;
+using UserService.Users.Infrastructure.Exceptions;
 
 namespace UserService.Users.Infrastructure.Repositories;
 
@@ -22,14 +25,34 @@ public class UserRepository : IUserRepository
 
     public async Task<User?> GetUserByIdAsync(long userId, CancellationToken ct)
     {
-        string sql = "SELECT " +
-                     "\"first_name\" as \"FirstName\"," +
-                     "\"last_name as \"LastName\"" +
-                     "\"email\" as Email" +
-                     "\"phone_number\" as PhoneNumber " +
-                     "FROM \"users\"" +
-                     "WHERE user_id = @userId";
-        var user = await _dbContext.DbConnection.QueryFirstAsync<User>(sql, new { userId = userId });
-        return user;
+        try
+        {
+            string sql = "SELECT " +
+                         "\"first_name\" as \"FirstName\"," +
+                         "\"last_name as \"LastName\"" +
+                         "\"email\" as Email" +
+                         "\"phone_number\" as PhoneNumber " +
+                         "FROM \"users\"" +
+                         "WHERE user_id = @userId";
+            var user = await _dbContext.DbConnection.QueryFirstAsync<User>(sql, new { userId = userId });
+            return user;
+        }
+        catch (NpgsqlException ex) when (ex.InnerException is SocketException)
+        {
+            _logger.LogCritical(ex, "Database connection failed while fetching the user {userId}.",
+                userId);
+            throw new InfrastructureException("Unable to reach the database", ex);
+        }
+        catch (NpgsqlException ex) when (ex.InnerException is TimeoutException)
+        {
+            _logger.LogError(ex, "Database query timed out while fetching user {UserId}", userId);
+            throw new InfrastructureException("Database operation timed out.", ex);
+
+        }
+        catch (PostgresException ex)
+        {
+            _logger.LogError(ex,"Database rejected the query while fetching the user {userId}.",userId);
+            throw new InfrastructureException("DataBase query failed", ex);
+        }
     }
 }
