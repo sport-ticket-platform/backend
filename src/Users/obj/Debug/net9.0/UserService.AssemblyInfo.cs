3c145366456f017674497a121221cebe8b253842
mohammad <mmdcj8499@gmail.com>
2026-07-18 10:38:11 +0330

    feat: defining the proto contract file

--- src/Users/obj/Debug/net9.0/UserService.AssemblyInfo.cs
+++ src/Users/obj/Debug/net9.0/UserService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21543e6304bcea9e3e869ecf06c385ab842fcc4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b087b6cf1be66e212d397fdd24daec0d786f2fc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
