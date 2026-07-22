3713a7b112f2f541ebbab97c3935ee43f798e4ed
mohammad <mmdcj8499@gmail.com>
2026-07-22 16:25:18 +0330

    feat: add venue domain model

--- src/Events/EventService/Events.Domain/Models/Venue.cs
+++ src/Events/EventService/Events.Domain/Models/Venue.cs
@@ -1,6 +1,33 @@
+using EventService.Events.Domain.Exceptions;
+using EventService.Events.Domain.ValueObjects;
+
 namespace EventService.Events.Domain.Models;
 
 public class Venue
 {
-    
+    public int VenueId { get; private set; }
+    public string Name { get; private set; }
+    public Address Address { get; private set; }
+
+
+    private Venue(int venueId, string name, Address address)
+    {
+        VenueId = venueId;
+        Name = name;
+        Address = address;
+    }
+
+    public Venue Create(int venueId, string name, Address address)
+    {
+        if (int.IsNegative(venueId))
+            throw new DomainException("The venue ID must be positive");
+
+        if (string.IsNullOrWhiteSpace(name))
+            throw new DomainException("The venue name must be a valid name");
+
+        ArgumentNullException.ThrowIfNull(address);
+
+        return new Venue(venueId, name, address);
+
+    }
 }
