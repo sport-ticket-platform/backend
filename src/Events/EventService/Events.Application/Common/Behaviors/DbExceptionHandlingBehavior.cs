440e5cc7d9d9bcef46ad22b816179924d854e901
mohammad <mmdcj8499@gmail.com>
2026-07-23 20:34:07 +0330

    add a new behavior for catching common exeptions when in case of DB call

--- src/Events/EventService/Events.Application/Common/Behaviors/DbExceptionHandlingBehavior.cs
+++ src/Events/EventService/Events.Application/Common/Behaviors/DbExceptionHandlingBehavior.cs
@@ -8,5 +8,37 @@ using Npgsql;
 public class DbExceptionHandlingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
-   
+    private ILogger<DbExceptionHandlingBehavior<TRequest, TResponse>> _logger;
+
+    public DbExceptionHandlingBehavior(ILogger<DbExceptionHandlingBehavior<TRequest, TResponse>>
+        logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (NpgsqlException ex) when (ex.InnerException is IOException)
+        {
+            _logger.LogCritical(ex, "Database connection failed.");
+            throw new InfrastructureException("Unable to reach the database", ex);
+        }
+        catch (NpgsqlException ex) when (ex.InnerException is TimeoutException)
+        {
+            _logger.LogError(ex, "Database query timed out.");
+            throw new InfrastructureException("Database operation timed out.", ex);
+        }
+        catch (PostgresException ex)
+        {
+            _logger.LogError(ex, "Database rejected the query.");
+            throw new InfrastructureException("DataBase query failed", ex);
+        }
+    }
 }
