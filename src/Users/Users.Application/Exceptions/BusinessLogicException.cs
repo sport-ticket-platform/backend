fcf7d800f30256a858fb70641357def12d892676
mohammad <mmdcj8499@gmail.com>
2026-07-24 10:41:27 +0330

    refactor: extending exception handling middleware to cover more general exceptions

---
+++ src/Users/Users.Application/Exceptions/BusinessLogicException.cs
@@ -0,0 +1,9 @@
+namespace UserService.Users.Application.Exceptions;
+
+public class BusinessLogicException : Exception
+{
+    public BusinessLogicException() : base(){}
+    public BusinessLogicException(string message) : base(message){}
+    public BusinessLogicException(string message,Exception innerException) : base(message,innerException){}
+
+}
